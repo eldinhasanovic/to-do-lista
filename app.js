54aa8b0387d780a001b637f1b7a610efb5224848
eldinhasanovic <almirhasanovic40@gmail.com>
2023-04-05 12:15:48 +0200

    gotovo

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
 var cilj = document.getElementsByTagName("LI");
 var lista = document.querySelector("ul");
-var filter = document.querySelector("select").value;
-function filter() {
-  for (i = 0; i < cilj.length; i++) {
-    if (filter == "done") {
-      if (cilj[i].style.className == "linija") {
-        cilj[i].style.display = "none";
-      } else if (filter == "notdone") {
-        if (cilj[i].style.className == "") {
-          cilj[i].style.display = "none";
-        }
-      }
-    }
-  }
-}
+var container = document.querySelector(".todoContainer");
 function dodajdugme(el) {
   var dugme = document.createElement("button");
   dugme.className = "obrisi";
@@ -29,7 +16,7 @@ function linija() {
   } else {
     event.target.className = "linija";
   }
-  console.log(event.target);
+  // console.log(event.target);
   // console.log(input);
 }
 for (i = 0; i < cilj.length; i++) {
@@ -46,4 +33,46 @@ function dodaj() {
 ugasi = document.getElementsByClassName("obrisi");
 // for (i = 0; i < ugasi.length; i++) {
 //   ugasi[i].setAttribute("onclick", "obrisi()");
+// }
+
+// var filterdugme = document.querySelector(".options");
+
+// filterdugme.addEventListener("click", function () {
+//   var filteri = document.querySelector("select.options");
+//   var filter = filteri.value;
+//   console.log("selektovana vrednost je " + filter);
+//   const allItems = Array.from(container.children);
+//   if (filter === "notdone") {
+//     allItems.map((item) => (item.style.display = ""));
+//     const uncompleted = allItems.filter(
+//       (item) => item.style.textDecoration !== "line-through"
+//     );
+//     uncompleted.forEach((item) => (item.style.display = "none"));
+//   } else if (filter === "done") {
+//     allItems.map((item) => (item.style.display = ""));
+//     const completed = allItems.filter(
+//       (item) => item.style.textDecoration === "line-through"
+//     );
+//     completed.forEach((item) => (item.style.display = "none"));
+//   } else {
+//     allItems.map((item) => (item.style.display = ""));
+//   }
+// });
+
+// var filteri = document.querySelector("select.options");
+// var filter = filteri.value;
+// function filter() {
+//   for (i = 0; i < cilj.length; i++) {
+//     if (filter == "done") {
+//       if (cilj[i].style.className == "linija") {
+//         cilj[i].style.display = "none";
+//       } else if (filter == "notdone") {
+//         if (cilj[i].style.className !== "linija") {
+//           cilj[i].style.display = "none";
+//         } else if (filter == "all") {
+//           cilj[i].style.className = "";
+//         }
+//       }
+//     }
+//   }
 // }
